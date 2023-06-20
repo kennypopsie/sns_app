aff7d97268d41e12b6be0e85628f0500986f94cb
kensuke <mynameiskensuke@gmail.com>
2023-06-20 01:48:13 +0000

    課題07-1,07-2

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # PostController
    get 'posts/new',to: 'posts#new', as: 'new_post'
    get 'posts/edit',to: 'posts#edit', as: 'edit_post'
-   get 'posts/', to: 'posts#index', as: 'index_post'
+   get 'posts/index', to: 'posts#index', as: 'index_post'
  
    get 'topics/new',to: 'topics#new', as: 'new_topics'
    get 'topics/edit',to: 'topics#edit', as: 'edit_topics'
