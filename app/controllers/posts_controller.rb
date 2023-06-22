65b40928928c85392c98307b82d7b48793f64eee
kensuke <mynameiskensuke@gmail.com>
2023-06-22 01:21:41 +0000

    課題09-3~6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
+ before_action :authenticate_user!
   def new 
     render :new
   end 
   
   def index
     render :index
+  end 
+  
+  def create
+    redirect_to new_post_path
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # PostController
+   get '/', to: 'posts#index', as: 'index_post'
    get 'posts/new',to: 'posts#new', as: 'new_post'
+   post 'posts/new', to: 'posts#create', as: 'create_post'
    get 'posts/edit',to: 'posts#edit', as: 'edit_post'
-   get 'posts/index', to: 'posts#index', as: 'index_post'
  
    get 'topics/new',to: 'topics#new', as: 'new_topics'
+   post 'topics/new',to: 'topics#create', as: 'create_topics'
    get 'topics/edit',to: 'topics#edit', as: 'edit_topics'
-
+   post 'topics/edit',to: 'topics#update', as: 'update_topics'
 end 
 
