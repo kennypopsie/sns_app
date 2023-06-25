1e0d40604fb5d4c9976a08e1b610687fdf300772
kensuke <mynameiskensuke@gmail.com>
2023-06-25 07:52:50 +0000

    RoR 10-4,5

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,26 @@
 class TopicsController < ApplicationController
  def new
+  @topic = Topic.new
    render :new
  end 
  
  def create
-   redirect_to edit_post_path
+  @topic = Topic.new(post_params)
+ 
+  if @topic.save
+   redirect_to edit_post_path, notice:'登録しました'
+  else
+   render :new, status: :unprocessable_entity
+  end
  end 
+ 
+ private
+ def post_params
+  params.require(:topic).permit(:title, :body, :image)
+ end 
+ # def create
+ #   redirect_to edit_post_path
+ # end 
  
  def edit
    render :edit
---
+++ app/models/topic.rb
@@ -0,0 +1,5 @@
+class Topic < ApplicationRecord
+  #validationを入力する
+  #タイトル(title): 必須, 最大100文字以内
+  validates :title, length: { maximum: 100 }, presence: true
+end
